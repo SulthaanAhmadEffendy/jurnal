54ab82f315928702912443e16f3122dd8ce4d239
SulthaanAhmadEffendy <sulthaneffendy@gmail.com>
2024-07-10 11:41:34 +0700

    authentication

---
+++ src/Categories/Index.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Index() {
+  const [categories, setCategories] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://journal.bariqfirjatullah.pw/api/category',
+        {
+          headers: { Accept: 'application/json' },
+        }
+      );
+      setCategories(data.data || []);
+    } catch (error) {
+      console.error('Failed to fetch categories:', error);
+      setCategories([]);
+    }
+  };
+
+  const deleteCategory = async (id) => {
+    try {
+      await axios.delete(
+        `https://journal.bariqfirjatullah.pw/api/category/${id}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        }
+      );
+      setMessage('Category deleted successfully.');
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+      fetchCategories();
+    } catch (error) {
+      console.log('Error deleting category:', error);
+    }
+  };
+
+  const handleDeleteClick = (id) => {
+    if (window.confirm('Are you sure to deleting this category')) {
+      deleteCategory(id);
+    }
+  };
+
+  return (
+    <div className=''>
+      <h2 className='text-center text-neutral-950 m-5 font-bold'>
+        CRUD CATEGORY
+      </h2>
+      <div className='container mx-auto'>
+        <div className='flex justify-center mb-3'>
+          <Link
+            to='add'
+            className='inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'
+          >
+            Add New Category
+          </Link>
+        </div>
+        <div className='overflow-x-auto flex justify-center'>
+          <table className='bg-white shadow-md rounded-lg overflow-hidden'>
+            <thead className='bg-gray-800 text-white'>
+              <tr>
+                <th className='py-3 px-4 text-left'>Kategori Pekerjaan</th>
+                <th className='py-3 px-4 text-left'>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {categories.length ? (
+                categories.map((item) => (
+                  <tr key={item.id} className='border-t'>
+                    <td className='py-3 px-4'>{item.name}</td>
+                    <td className='py-3 px-4'>
+                      <Link
+                        to={`/edit/${item.id}`}
+                        className='text-yellow-300 hover:text-yellow-700 mr-4'
+                      >
+                        Edit
+                      </Link>
+                      <button
+                        onClick={() => handleDeleteClick(item.id)}
+                        className='text-red-600 hover:text-red-800'
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan='2' className='text-center py-4'>
+                    No categories found.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {message && <p className='text-green-500 text-center mt-4'>{message}</p>}
+    </div>
+  );
+}
+
+export default Index;
---
+++ src/routes.jsx
@@ -0,0 +1,67 @@
+import {
+  FolderIcon,
+  UserCircleIcon,
+  TableCellsIcon,
+  InformationCircleIcon,
+  ServerStackIcon,
+  RectangleStackIcon,
+} from '@heroicons/react/24/solid';
+import { Home, Profile, Tables, Notifications } from '@/pages/dashboard';
+import { SignIn, SignUp } from '@/pages/auth';
+import Index from './Categories/Index';
+
+const icon = {
+  className: 'w-5 h-5 text-inherit',
+};
+
+export const routes = [
+  {
+    layout: 'dashboard',
+    pages: [
+      {
+        icon: <FolderIcon {...icon} />,
+        name: 'Categories',
+        path: '/categories',
+        element: <Index />,
+      },
+      {
+        icon: <UserCircleIcon {...icon} />,
+        name: 'profile',
+        path: '/profile',
+        element: <Profile />,
+      },
+      {
+        icon: <TableCellsIcon {...icon} />,
+        name: 'tables',
+        path: '/tables',
+        element: <Tables />,
+      },
+      {
+        icon: <InformationCircleIcon {...icon} />,
+        name: 'notifications',
+        path: '/notifications',
+        element: <Notifications />,
+      },
+    ],
+  },
+  {
+    title: 'auth pages',
+    layout: 'auth',
+    pages: [
+      {
+        icon: <ServerStackIcon {...icon} />,
+        name: 'sign in',
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        icon: <RectangleStackIcon {...icon} />,
+        name: 'sign up',
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+    ],
+  },
+];
+
+export default routes;
